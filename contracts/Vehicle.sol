f753d20ee6fe3aeade94ea036793d123b159a77f
Sebastian Gabriel Savu <sebi_saske@yahoo.com>
2021-10-30 15:53:47 +0100

    v0.1.0
    - Added access control functionality to contract allowing the use of a minter role
    - Added minting functionality to frontend and retrieving and displaying user owned tokens

--- contracts/Vehicle.sol
+++ contracts/Vehicle.sol
@@ -5,23 +5,35 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract Vehicle is ERC721Enumerable {
-
+contract Vehicle is ERC721Enumerable, Ownable, AccessControl {
     using Counters for Counters.Counter;
     Counters.Counter _tokenIds;
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
     uint256[] private _allTokens;
     mapping(address => mapping(uint256 => uint256)) private _ownedTokens;
     mapping(uint256 => string) _tokenURIs;
 
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721Enumerable, AccessControl)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
 
     struct VehicleToken {
         uint256 id;
         string uri;
     }
 
-    constructor() ERC721("Vehicle", "VHC") {}
+    constructor() ERC721("Vehicle", "VHC") {
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _setupRole(MINTER_ROLE, msg.sender);
+    }
 
     /*
     modifier notRegistered(uint256 tokenId) {
@@ -45,13 +57,11 @@ contract Vehicle is ERC721Enumerable {
         return _tokenURIs[tokenId];
     }
 
-    function mint(string memory uri) public returns (uint256){
-
+    function mint(string memory uri) public onlyRole(MINTER_ROLE) returns (uint256) {
         uint256 _id = _tokenIds.current();
         _mint(msg.sender, _id);
         _setTokenURI(_id, uri);
         _tokenIds.increment();
         return _id;
     }
-
 }
--- src/App.js
+++ src/App.js
@@ -2,13 +2,53 @@ import React, { Component } from 'react';
 import Web3 from 'web3'
 import Vehicle from './abis/Vehicle.json'
 import { render } from 'react-dom';
+import Dropzone from 'react-dropzone'
+import { create } from "ipfs-http-client";
+
+const ipfsClient = create("https://ipfs.infura.io:5001/api/v0");
+const ipfsBaseUrl = "https://ipfs.infura.io/ipfs/";
+
 
 class App extends Component {
+
+
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      contract: null,
+      web3: null,
+      buffer: null,
+      account: null,
+      tokens: [],
+      NFTS: [],
+    }
+  }
+
+
+  ayee = (prm) => {
+
+    this.setState({
+      NFTS: [...this.state.NFTS, prm]
+    })
+
+    console.log(this.state.NFTS)
+  }
+
+  fetchMetatDataForNFTS = () => {
+    this.state.tokens.forEach((nft) => {
+      fetch(nft)
+        .then(response => response.json())
+        .then(data => this.ayee(data));
+
+    });
+  };
+
 
   async componentWillMount() {
     await this.loadWeb3()
     await this.loadBlockchainData()
-    await this.displayMyTokens()
   }
 
   async loadWeb3() {
@@ -25,114 +65,146 @@ class App extends Component {
   }
 
   async loadBlockchainData() {
-
     const web3 = window.web3
+    // Load account
     const accounts = await web3.eth.getAccounts()
-
-    window.ethereum.on('accountsChanged', () => {
-      window.location.reload()
-    })
-
     this.setState({ account: accounts[0] })
-
     const networkId = await web3.eth.net.getId()
     const networkData = Vehicle.networks[networkId]
     if (networkData) {
-      const abi = Vehicle.abi
-      const address = networkData.address
-      const contract = new web3.eth.Contract(abi, address)
+      const contract = new web3.eth.Contract(Vehicle.abi, networkData.address)
       this.setState({ contract })
-      const totalSupply = await contract.methods.totalSupply().call()
-      this.setState({ totalSupply })
-      const myBalance = await contract.methods.balanceOf(this.state.account).call()
-      this.setState({ myBalance })
 
     } else {
       window.alert('Smart contract not deployed to detected network.')
     }
-  }
 
-  async displayMyTokens() {
+    console.log(this.state.account, this.state.contract)
 
-    for (var i = 0; i < this.state.myBalance; i++) {
-      const vehicle = await this.state.contract.methods.tokenOfOwnerByIndex(this.state.account, i).call()
-      const uri = await this.state.contract.methods.tokenURI(vehicle).call()
+    window.ethereum.on("accountsChanged", (accounts) => {
+      window.location.reload();
+    });
+    window.ethereum.on("chainChanged", () => {
+      window.location.reload();
+    });
+
+    const myBalance = await this.state.contract.methods.balanceOf(this.state.account).call()
+
+    for (var i = 0; i < myBalance; i++) {
+      let token = await this.state.contract.methods.tokenOfOwnerByIndex(this.state.account, i).call()
+      let uri = await this.state.contract.methods.tokenURI(token).call()
       this.setState({
-        vehicles: [...this.state.vehicles, uri]
+        tokens: [...this.state.tokens, uri]
       })
     }
-    
+   
+    this.fetchMetatDataForNFTS(this.state.tokens)
+
   }
 
-  mint = (vehicle) => {
-    this.state.contract.methods.mint(vehicle).send({ from: this.state.account })
-      .once('receipt', (receipt) => {
-        this.setState({
-          vehicles: [...this.state.vehicles, vehicle]
-        })
+  mint = (_uri) => {
+    this.state.contract.methods
+      .mint(_uri)
+      .send({ from: this.state.account })
+      .once("error", (err) => {
+        console.log(err);
+        //setLoading(false);
+        //setStatus("Error");
       })
-  }
+      .then((receipt) => {
+        console.log(receipt);
+        //setLoading(false);
+        //dispatch(fetchData(blockchain.account));
+        //setStatus("Successfully minting your NFT");
+      });
+  };
 
-  constructor(props) {
-    super(props)
-    this.state = {
-      account: '',
-      contract: null,
-      totalSupply: 0,
-      myBalance: 0,
-      vehicles: []
+
+  startMintingProcess = () => {
+    const color = this.createAttribute("Color", "black")
+    const body = this.createAttribute("Body", "black")
+    const transmission = this.createAttribute("Transmission", "black")
+    const mileage = this.createAttribute("Transmission", "black")
+    const fuel = this.createAttribute("Transmission", "black")
+    const engine = this.createAttribute("Transmission", "black")
+    const doors = this.createAttribute("Transmission", "black")
+    const seats = this.createAttribute("Transmission", "black")
+    const attributes = [color, body, transmission, mileage, fuel, engine, doors, seats]
+    this.createMetaDataAndMint("aye", "ayeee", this.state.buffer, attributes);
+  };
+
+  createAttribute = (type, value) => {
+    return {
+      trait_type: type,
+      value: value
     }
   }
 
+  createMetaDataAndMint = async (_name, _des, _imgBuffer, _attributes) => {
+    // setLoading(true);
+    //setStatus("Uploading to IPFS");
+    try {
+      const addedImage = await ipfsClient.add(_imgBuffer);
+
+      const metaDataObj = {
+        name: _name,
+        description: _des,
+        image: ipfsBaseUrl + addedImage.path,
+        attributes: _attributes
+      };
+      const addedMetaData = await ipfsClient.add(JSON.stringify(metaDataObj));
+      console.log(ipfsBaseUrl + addedMetaData.path);
+      this.mint(ipfsBaseUrl + addedMetaData.path);
+    } catch (err) {
+      console.log(err);
+      //setLoading(false);
+      //("Error");
+    }
+  };
+
+  onDrop = (file) => {
+
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      
+      console.log(reader.result)
+      this.setState({ buffer: Buffer(reader.result.split(",")[1],"base64") })
+      console.log('buffer', this.state.buffer)
+    };
+    reader.readAsDataURL(file[0]);
+
+  }
 
   render() {
     return (
-      <div>
-        <nav>
-          <a>
-            Vehicles
-          </a>
-          <ul>
-            <small><span>{this.state.account}</span></small>
-          </ul>
-        </nav>
+      <div className="text-center mt-5">
+        <Dropzone onDrop={this.onDrop}>
+          {({ getRootProps, getInputProps, isDragActive }) => (
+            <div {...getRootProps()}>
+              <input {...getInputProps()} />
+              {isDragActive ? "Drop it like it's hot!" : 'Click me or drag a file to upload!'}
+            </div>
+          )}
+        </Dropzone>
+        <button onClick={this.startMintingProcess}>
+          MINT
+        </button>
         <div>
-          <div>
-            <main role="main">
-              <div >
-                <h1>Issue Vehicle</h1>
-                <form onSubmit={(event) => {
-                  event.preventDefault()
-                  const vehicle = this.vehicle.value
-                  this.mint(vehicle)
-                }}>
-                  <input
-                    type='text'
-                    placeholder='e.g. https://sgsavu.com'
-                    ref={(input) => { this.vehicle = input }}
-                  />
-                  <input
-                    type='submit'
-                    value='MINT'
-                  />
-                </form>
+          {this.state.NFTS.map((NFT, key) => {
+            return (
+              <div key={key}>
+                <div>Name: {NFT.name}</div>
+                <div>Description: {NFT.description}</div>
+                <div>Image: 
+                <img src={NFT.image} width="100px" height="100px" /> </div>
+               
               </div>
-            </main>
-          </div>
-          <hr />
-          <div>
-            {this.state.vehicles.map((vehicle, key) => {
-              return (
-                <div key={key}>
-                  <div>{vehicle}</div>
-                </div>
-              )
-            })}
-          </div>
+            )
+          })}
         </div>
       </div>
     );
   }
-
 }
+
 export default App;
